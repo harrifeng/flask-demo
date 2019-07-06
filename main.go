25f3afb654d0869e4ae39e0d272e951db7c02841
Haoran Feng <harrifeng@gmail.com>
2019-07-06 16:40:21 +0800

    new port

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func main() {
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8088", nil)
 }
